09a403efa68d817e461ee7e8b1ef3f43addd9e4f
Nakul Iyer <nakulpiyer@gmail.com>
2022-12-02 13:37:16 -0600

    Two skeleton methods

---
+++ implementation.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def two_state_phylo(M: List[List[int]]) -> bool:
+    """Determines whether matrix M accepts a perfect phylogeny"""
+    pass
+
+def three_state_phylo(M: List[List[int]]) -> bool:
+    """Determines whether matrix M accepts a perfect phylogeny"""
+    pass
